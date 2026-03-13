0115520296dbd1cf4cfc997c943cf0d7e2c67fb2
CCollins1016 <ccollins@bsd220.org>
2026-03-13 10:44:20 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Christian Collins on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
